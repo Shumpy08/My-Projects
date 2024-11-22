35f499e841b74e542a0eca8e8496b7da3615bf60
Shubham Sundaram <shumpy0808@gmail.com>
2024-11-22 10:22:50 +0530

    Updated main.py

--- Day 01 Project - Band Name Generator/main.py
+++ Day 01 Project - Band Name Generator/main.py
@@ -1,4 +1,4 @@
-print("Welcome to Band Name Generator.")
+print("Welcome to Shumpy's Band Name Generator.")
 city = input("Which city did you grow up in?: ")
 pet = input("What is the name of your pet?: ")
 print('Your band name could be ' + city + " " + pet)
